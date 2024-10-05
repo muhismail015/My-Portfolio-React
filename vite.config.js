47f3fbdb12b590377d3fa910e1a7e89d7e8223f3
hasyim404 <muhaisim7@gmail.com>
2024-10-05 19:58:29 +0700

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  appType: 'mpa'
   // buat namanya biar gk random
   // build: {
   //   rollupOptions: {
